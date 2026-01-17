0f517c7a8281f953d8b9ed05527cb51dadd1f7a7
Amaan-29 <amaanbarge@gmail.com>
2026-01-17 13:09:42 -0500

    RemoveNth node HW

---
+++ RemoveNthFromEnd.java
@@ -0,0 +1,32 @@
+public class RemoveNthFromEnd {
+      public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int count=0;
+        //make a dummy node which is one node previous to head to capture edge case
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        //Start pointers from dummy
+        ListNode slow=dummy;
+        ListNode fast=dummy;
+        //move the fast pointer until it reaches n length
+        while(count<=n){
+            fast=fast.next;
+            count++;
+        }
+        //Now slide both the pointers by 1 as the n distance btwn slow and fast are captured
+        while(fast!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        ListNode temp=slow.next;
+        slow.next=slow.next.next;
+        temp.next=null;
+        return dummy.next;
+    }
+}
